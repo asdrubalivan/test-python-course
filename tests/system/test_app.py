cb127152de21dfa9ba109764d5e588bd9bd262b5
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-09-16 13:49:38 -0300

    Finished tests

--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -6,6 +6,10 @@ from post import Post
 
 
 class AppTest(TestCase):
+    def setUp(self):
+        blog = Blog("Test", "Test Author")
+        app.blogs = {"Test": blog}
+
     def test_menu_calls_create_blog(self):
         with patch("builtins.input") as mocked_input:
             mocked_input.side_effect = ("c", "Test Create Blog", "Test Author", "q")
@@ -24,8 +28,6 @@ class AppTest(TestCase):
                 mocked_print_blog.assert_called()
 
     def test_print_blogs(self):
-        blog = Blog("Test", "Test Author")
-        app.blogs = {"Test": blog}
         with patch("builtins.print") as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with("- Test by Test Author (0 posts)")
@@ -37,17 +39,15 @@ class AppTest(TestCase):
             self.assertIsNotNone(app.blogs.get("Test"))
 
     def test_ask_read_blog(self):
-        blog = Blog("Test", "Test Author")
-        app.blogs = {"Test": blog}
+        blog = app.blogs["Test"]
         with patch("builtins.input", return_value="Test"):
             with patch("app.print_posts") as mocked_print_posts:
                 app.ask_read_blog()
                 mocked_print_posts.assert_called_with(blog)
 
     def test_print_posts(self):
-        blog = Blog("Test", "Test Author")
+        blog = app.blogs["Test"]
         blog.create_post("Test Post", "Test Content")
-        app.blogs = {"Test": blog}
         with patch("app.print_post") as mocked_print_post:
             app.print_posts(blog)
             mocked_print_post.assert_called_with(blog.posts[0])
@@ -64,8 +64,7 @@ Post content
             mocked_print.assert_called_with(expected_print)
 
     def test_ask_create_blog(self):
-        blog = Blog("Test", "Test Author")
-        app.blogs = {"Test": blog}
+        blog = app.blogs["Test"]
         with patch("builtins.input") as mocked_input:
             mocked_input.side_effect = ("Test", "Test Title", "Test Content")
             app.ask_create_post()
